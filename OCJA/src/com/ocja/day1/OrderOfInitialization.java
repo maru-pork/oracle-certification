eaf27840af6342a1e7a1795b572b082025e1d919
rufomaryann <shajeemaru@gmail.com>
2016-07-12 21:20:51 +0800

    initial commit

---
+++ OCJA/src/com/ocja/day1/OrderOfInitialization.java
@@ -0,0 +1,49 @@
+package com.ocja.day1;
+
+public class OrderOfInitialization {
+	
+	// constructor
+	public OrderOfInitialization() {
+		System.out.println("constructor");
+	}
+	
+	// static initializer
+	static {
+		System.out.println("static");
+	}
+
+	static A  sa1 = new A(1);
+	A a = new A(2);
+		
+	public static void print() {
+		System.out.println("print");
+	}
+	
+	public static void main(String[] args) {
+		OrderOfInitialization b = new OrderOfInitialization();
+		A a = new A(3);		
+		/*
+		 * static
+		 * 1
+		 * 4
+		 * 2
+		 * constructor
+		 * 3
+		 * */
+	}
+	
+	static A sa2 = new A(4);
+
+}
+
+class A {
+	
+	public A() {
+		
+	}
+	
+	 public A(int i) {
+		 System.out.println(i);
+	 }
+	
+}
